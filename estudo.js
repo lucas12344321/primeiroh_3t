e8239a4c04818e65ff9f51bacb736c6e303600dc
lucas12344321 <110060847+lucas12344321@users.noreply.github.com>
2022-09-28 18:31:42 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -2,6 +2,19 @@
 //console.log("oi,eu estou no console");
 //document.write("aula de Pens.Comp.");
 
-function mechame(){
-    alert("Oi,eu sou um alert");
+let tabuada = 8;
+
+function mechame(nome){
+    document.write("<h1>tabuada do " +tabuada + "</h1>")
+    document.write(tabuada + " x 1 = " + (tabuada*1)+"<Br>");
+    document.write(tabuada + " x 2 = " + (tabuada*2)+"<Br>");
+    document.write(tabuada + " x 3 = " + (tabuada*3)+"<Br>");
+    document.write(tabuada + " x 4 = " + (tabuada*4)+"<Br>");
+    document.write(tabuada + " x 5 = " + (tabuada*5)+"<Br>");
+    document.write(tabuada + " x 6 = " + (tabuada*6)+"<Br>");
+    document.write(tabuada + " x 7 = " + (tabuada*7)+"<Br>");
+    document.write(tabuada + " x 8 = " + (tabuada*8)+"<Br>");
+    document.write(tabuada + " x 9 = " + (tabuada*9)+"<Br>");
+    document.write(tabuada + " x 10 = " + (tabuada*10)+"<Br><Br>");
+    document.write("feita por " + nome);
 }
